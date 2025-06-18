927ad80f98cc857ab3d6cdf6e41a433816f03aa8
Sefedeam <sergio.delgado.a@utec.edu.pe>
2025-06-18 01:09:32 -0500

    Se incorporó el archivo .dockerignore

--- app/core/config.py
+++ app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     DATABASE_NAME: str = ""
     SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_HOURS: int = 72
+    ACCESS_TOKEN_EXPIRE_HOURS: int = 672
     ALLOWED_COUNTRIES: str
     MAX_ADDRESSES_PER_USER: int
     SMTP_HOST: str
